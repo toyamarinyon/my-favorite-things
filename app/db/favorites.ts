da83e84fcb9c802711c8a736e09246bd9d53a333
satoshi toyama <satoshi.toyama@route06.co.jp>
2024-02-15 21:57:49 +0900

    update

--- app/db/favorites.ts
+++ app/db/favorites.ts
@@ -2,8 +2,11 @@ import { createId } from "@paralleldrive/cuid2";
 import { sql } from "drizzle-orm";
 import { sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { createInsertSchema } from "drizzle-valibot";
-import { merge, minLength, object, string } from "valibot";
+import { Input, merge, minLength, object, picklist, string } from "valibot";
 import { users } from "./users";
+
+export const BodyTypeSchema = picklist(["image", "text"]);
+export type BodyType = Input<typeof BodyTypeSchema>;
 /**
  * A favorite is a user's bookmark of a specific piece of content.
  * content: The content of the favorite. If the favorite is a picture, this will be the object id. If the favorite is a note, this will be the note content.
@@ -15,6 +18,10 @@ export const favorites = sqliteTable("favorites", {
 	userId: text("user_id")
 		.notNull()
 		.references(() => users.id),
+	bodyType: text("body_type")
+		.$type<Input<typeof BodyTypeSchema>>()
+		.notNull()
+		.default("image"),
 	title: text("title").notNull(),
 	body: text("body").notNull(),
 	referenceUrl: text("reference_url"),
--- app/routes/favorites.new.tsx
+++ app/routes/favorites.new.tsx
@@ -5,18 +5,21 @@ import "cropperjs/dist/cropper.css";
 import { SparkleIcon } from "lucide-react";
 import { useCallback, useEffect, useRef, useState } from "react";
 import Cropper, { ReactCropperElement } from "react-cropper";
-import { ValiError, flatten } from "valibot";
+import { match } from "ts-pattern";
+import { ValiError, flatten, parse } from "valibot";
 import { FileInput } from "~/components/favorite/file-input";
 import { MainLayout } from "~/components/layout/main";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "~/components/ui/radio-group";
 import {
 	Tooltip,
 	TooltipContent,
 	TooltipProvider,
 	TooltipTrigger,
 } from "~/components/ui/tooltip";
+import { BodyType, BodyTypeSchema } from "~/db/favorites";
 import { createFavorite } from "~/functions/favorites/createFavorite.server";
 import type { action as analyzeImageAction } from "~/routes/analyzeImage";
 
@@ -115,105 +118,170 @@ export default function FavoriteNewPage() {
 		inputRef.current.value = analyzeImage.data.analyze;
 	}, [analyzeImage.data]);
 	const navigation = useNavigation();
+	const [favoriteType, setFavoriteType] = useState<BodyType>("image");
+	const handleFavoriteTypeChange = useCallback((newFavoriteType: string) => {
+		parse(BodyTypeSchema, newFavoriteType);
+		setFavoriteType(parse(BodyTypeSchema, newFavoriteType));
+	}, []);
 	return (
 		<MainLayout>
 			<div className="p-4 font-thin">
 				<h1 className="text-lg">Add a new favorite</h1>
 				<div className="flex divide-x divide-slate-900">
 					<div className="pr-4">
-						<ol className="ml-6 list-decimal pt-2">
+						<ol className="ml-6 list-decimal pt-2 space-y-4">
 							<li>
-								<div className="h-16 space-y-0.5">
-									<p>Click the "Select a file" button to select a file</p>
-									<FileInput onFileChange={handleFileChange} />
+								<div>
+									<p className="mb-2">Chose the type you want to add</p>
+									<RadioGroup
+										value={favoriteType}
+										onValueChange={handleFavoriteTypeChange}
+									>
+										<div className="flex items-center space-x-2">
+											<RadioGroupItem value="image" id="image" />
+											<Label htmlFor="image">Image</Label>
+										</div>
+										<div className="flex items-center space-x-2">
+											<RadioGroupItem value="text" id="text" />
+											<Label htmlFor="text">Text</Label>
+										</div>
+									</RadioGroup>
 								</div>
 							</li>
-							<li>
-								<p>Crop the uploaded image and enter the details</p>
-								{flow.step === "enter-details" && (
-									<Form method="post">
-										<div className="mb-8 space-y-1">
-											<div className="w-[500px]">
-												<Cropper
-													src={flow.imageUrl}
-													style={{ width: "100%" }}
-													viewMode={1}
-													// Cropper.js options
-													initialAspectRatio={4 / 3}
-													aspectRatio={4 / 3}
-													guides={false}
-													background={false}
-													ready={handleCrop}
-													cropend={handleCrop}
-													ref={cropperRef}
-													zoomable={false}
-												/>
-											</div>
-										</div>
 
-										<div>
-											<Label htmlFor="title">Title</Label>
-											<div className="flex items-center space-x-1">
-												<Input
-													type="text"
-													id="title"
-													name="title"
-													ref={inputRef}
-												/>
-												<TooltipProvider>
-													<Tooltip>
-														<TooltipTrigger asChild>
-															<Button
-																type="button"
-																size="form"
-																onClick={handleClickAnalyzeButton}
-															>
-																{analyzeImage.state === "submitting" && (
-																	<p className="sono text-xs">Analyzing...</p>
-																)}
-																<SparkleIcon
-																	className={clsx("h-4 w-4", {
-																		"animate-spin":
-																			analyzeImage.state === "submitting",
-																	})}
-																/>
-															</Button>
-														</TooltipTrigger>
-														<TooltipContent>
-															<p>Analyze images with AI</p>
-														</TooltipContent>
-													</Tooltip>
-												</TooltipProvider>
+							{match(favoriteType)
+								.with("image", () => (
+									<>
+										<li>
+											<div className="space-y-0.5">
+												<p>Click the "Select a file" button to select a file</p>
+												<FileInput onFileChange={handleFileChange} />
 											</div>
-										</div>
-										{cropped800WidthBase64Url != null && (
-											<input
-												name="dataUrl"
-												value={cropped800WidthBase64Url}
-												type="hidden"
-											/>
-										)}
-										<div className="mb-2">
-											<Label htmlFor="referenceUrl">URL</Label>
-											<Input
-												type="text"
-												id="referenceUrl"
-												name="referenceUrl"
-											/>
-											<p className="text-xs">
-												URL of the image reference (web page, book introduction
-												page, etc.)
-											</p>
-										</div>
-										<Button
-											type="submit"
-											progress={navigation.state === "loading"}
-										>
-											Add fav
-										</Button>
-									</Form>
-								)}
-							</li>
+										</li>
+										<li>
+											<p>Crop the uploaded image and enter the details</p>
+											{flow.step === "enter-details" && (
+												<Form method="post">
+													<div className="mb-8 space-y-1">
+														<div className="w-[500px]">
+															<Cropper
+																src={flow.imageUrl}
+																style={{ width: "100%" }}
+																viewMode={1}
+																// Cropper.js options
+																initialAspectRatio={4 / 3}
+																aspectRatio={4 / 3}
+																guides={false}
+																background={false}
+																ready={handleCrop}
+																cropend={handleCrop}
+																ref={cropperRef}
+																zoomable={false}
+															/>
+														</div>
+													</div>
+
+													<div>
+														<Label htmlFor="title">Title</Label>
+														<div className="flex items-center space-x-1">
+															<Input
+																type="text"
+																id="title"
+																name="title"
+																ref={inputRef}
+															/>
+															<TooltipProvider>
+																<Tooltip>
+																	<TooltipTrigger asChild>
+																		<Button
+																			type="button"
+																			size="form"
+																			onClick={handleClickAnalyzeButton}
+																		>
+																			{analyzeImage.state === "submitting" && (
+																				<p className="sono text-xs">
+																					Analyzing...
+																				</p>
+																			)}
+																			<SparkleIcon
+																				className={clsx("h-4 w-4", {
+																					"animate-spin":
+																						analyzeImage.state === "submitting",
+																				})}
+																			/>
+																		</Button>
+																	</TooltipTrigger>
+																	<TooltipContent>
+																		<p>Analyze images with AI</p>
+																	</TooltipContent>
+																</Tooltip>
+															</TooltipProvider>
+														</div>
+													</div>
+													{cropped800WidthBase64Url != null && (
+														<input
+															name="dataUrl"
+															value={cropped800WidthBase64Url}
+															type="hidden"
+														/>
+													)}
+													<div className="mb-2">
+														<Label htmlFor="referenceUrl">URL</Label>
+														<Input
+															type="text"
+															id="referenceUrl"
+															name="referenceUrl"
+														/>
+														<p className="text-xs">
+															URL of the image reference (web page, book
+															introduction page, etc.)
+														</p>
+													</div>
+													<Button
+														type="submit"
+														progress={navigation.state === "submitting"}
+													>
+														Add fav
+													</Button>
+												</Form>
+											)}
+										</li>
+									</>
+								))
+								.with("text", () => (
+									<>
+										<li>
+											<p>Enter the details</p>
+											<Form method="post">
+												<div className="mb-4">
+													<div>
+														<Label htmlFor="title">Title</Label>
+														<Input type="text" id="title" name="title" />
+													</div>
+													<div>
+														<Label htmlFor="referenceUrl">URL</Label>
+														<Input
+															type="text"
+															id="referenceUrl"
+															name="referenceUrl"
+														/>
+														<p className="text-xs">
+															URL of the image reference (web page, book
+															introduction page, etc.)
+														</p>
+													</div>
+												</div>
+												<Button
+													type="submit"
+													progress={navigation.state === "submitting"}
+												>
+													Add fav
+												</Button>
+											</Form>
+										</li>
+									</>
+								))
+								.exhaustive()}
 						</ol>
 					</div>
 				</div>
